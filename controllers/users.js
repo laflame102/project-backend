0ca4eb9270d573fa1c3000eaf71ee74b5687e8c8
OksanaTur <ksyturkevych@gmail.com>
2023-06-30 17:51:02 +0400

    Update users.js

--- controllers/users.js
+++ controllers/users.js
@@ -68,7 +68,7 @@ const refresh = async (req, res) => {
   const { authorization = "" } = req.headers;
   const [bearer, token] = authorization.split(" ");
   if (bearer !== "Bearer") {
-    next(HttpError(401));
+   throw HttpError(401, "Unauthorized");
   }
 
   try {
